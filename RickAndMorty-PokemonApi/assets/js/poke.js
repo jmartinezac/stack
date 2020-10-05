aa1023137682690787d251fdcf6806b52653bb19
jmartinezac <jim_2306@hotmail.com>
2020-10-05 16:10:55 -0500

    All

--- RickAndMorty-PokemonApi/assets/js/poke.js
+++ RickAndMorty-PokemonApi/assets/js/poke.js
@@ -1,5 +1,7 @@
 const idBuscar = document.getElementById("btn-buscar");
 
+const urlTop = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=00"
+
 idBuscar.addEventListener("click", function buscarPoke() {
   const idPoke = document.getElementById("buscar-poke").value;
   const url = `https://pokeapi.co/api/v2/pokemon/${idPoke}`;
@@ -24,13 +26,46 @@ const mostrarPokemon = (data) => {
   console.log(data);
   console.log(data.abilities[0].ability.name);
   let html = `<div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="${
-                      data.sprites.front_default
-                    }" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">${data.name.toUpperCase()}</h5>
-                        <p> Habilidad: ${data.abilities[0].ability.name}</p>
-                    </div>
-                </div>`;
+                      <img class="card-img-top" src="${
+                        data.sprites.front_default
+                      }" alt="Card image cap">
+                      <div class="card-body">
+                          <h5 class="card-title">${data.name.toUpperCase()}</h5>
+                          <p> Habilidad: ${data.abilities[0].ability.name}</p>
+                      </div>
+                  </div>`;
   document.getElementById("card-poke").innerHTML = html;
+
 };
+
+const getAll = (url) => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .then((pokeAll) => {
+      mostrarPokeAll(pokeAll)
+    })
+}
+
+const mostrarPokeAll = (data) => {
+  let html = ""
+  data.results.forEach((x) => {
+    const url = x.url
+    return fetch(url)
+      .then((resp) => resp.json())
+      .then((poke) => {
+        html += `<div class="card m-2 p-4" style="width: 18rem;">
+                  <img class="card-img-top" src="${
+                    poke.sprites.front_default
+                  }" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">${poke.name.toUpperCase()}</h5>
+                      <p> Habilidad: ${poke.abilities[0].ability.name}</p>
+                  </div>
+               </div>`;
+        document.getElementById("card-pokeAll").innerHTML = html;
+      })
+  });
+}
+
+
+getAll(urlTop)
